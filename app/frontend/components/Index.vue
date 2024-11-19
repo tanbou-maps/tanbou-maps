c86f9417011bab12d15caf7d2964032a4bea2c21
Hayate Aoki <aokihayate20030709@gmail.com>
2024-11-19 14:15:08 +0900

    feat: template の微調整

--- app/frontend/components/Index.vue
+++ app/frontend/components/Index.vue
@@ -45,8 +45,8 @@
       </section>
 
       <!-- Feature Section -->
-      <section class="bg-gray-900 py-12">
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <section class="bg-black py-12">
+        <div class="mx-auto max-w-7xl px-4 py-4 sm:px-6 lg:px-8">
           <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
             <div
               v-for="(feature, index) in features"
