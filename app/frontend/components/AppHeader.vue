104c358a191e2d6e0269841b5163d19f53669508
Hayate Aoki <aokihayate20030709@gmail.com>
2025-01-16 12:16:09 +0900

    feat: index ページのスタイルを変更

--- app/frontend/components/AppHeader.vue
+++ app/frontend/components/AppHeader.vue
@@ -1,47 +1,118 @@
 <template>
+  <!-- ========== HEADER ========== -->
   <header
-    class="fixed left-0 right-0 top-0 z-50 border-b border-gray-300 bg-white/80 backdrop-blur-md"
+    class="z-50 flex w-full flex-wrap py-7 md:flex-nowrap md:justify-start"
   >
-    <div
-      class="mx-auto flex max-w-7xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8"
+    <nav
+      class="relative mx-auto flex w-full max-w-7xl basis-full flex-wrap items-center px-4 md:grid md:grid-cols-12 md:px-6 md:px-8"
+      aria-label="Global"
     >
-      <h1 class="text-2xl font-bold">Tanbou Maps</h1>
-      <nav class="flex space-x-4">
-        <a
-          v-for="item in navigationItems"
-          :key="item.name"
-          :href="item.href"
-          :class="{
-            'text-blue-500': item.current,
-            'text-gray-700': !item.current,
-          }"
-          class="hover:text-blue-600"
+      <div class="md:col-span-3">
+        <!-- Logo -->
+        <!-- End Logo -->
+      </div>
+
+      <!-- Button Group -->
+      <div
+        class="ms-auto flex items-center gap-x-2 py-1 md:order-3 md:col-span-3 md:ps-6"
+      >
+        <button
+          type="button"
+          class="inline-flex items-center gap-x-2 rounded-xl border border-gray-200 px-3 py-2 text-sm font-medium text-black hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50"
         >
-          {{ item.name }}
-        </a>
-      </nav>
-      <img :src="avatarUrl" alt="User Avatar" class="h-8 w-8 rounded-full" />
-    </div>
+          Sign out
+        </button>
+        <button
+          type="button"
+          class="inline-flex items-center gap-x-2 rounded-xl border border-transparent bg-lime-400 px-3 py-2 text-sm font-medium text-black transition hover:bg-lime-500 focus:bg-lime-500 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+        >
+          Profile
+        </button>
+
+        <div class="md:hidden">
+          <button
+            type="button"
+            class="hs-collapse-toggle flex size-[38px] items-center justify-center rounded-xl border border-gray-200 text-sm font-semibold text-black hover:bg-gray-100 disabled:pointer-events-none disabled:opacity-50"
+            data-hs-collapse="#navbar-collapse-with-animation"
+            aria-controls="navbar-collapse-with-animation"
+            aria-label="Toggle navigation"
+          >
+            <svg
+              class="hs-collapse-open:hidden size-4 flex-shrink-0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <line x1="3" x2="21" y1="6" y2="6" />
+              <line x1="3" x2="21" y1="12" y2="12" />
+              <line x1="3" x2="21" y1="18" y2="18" />
+            </svg>
+            <svg
+              class="hs-collapse-open:block hidden size-4 flex-shrink-0"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path d="M18 6 6 18" />
+              <path d="m6 6 12 12" />
+            </svg>
+          </button>
+        </div>
+      </div>
+      <!-- End Button Group -->
+
+      <!-- Collapse -->
+      <div
+        id="navbar-collapse-with-animation"
+        class="hs-collapse hidden grow basis-full overflow-hidden transition-all duration-300 md:order-2 md:col-span-6 md:block md:w-auto md:basis-auto"
+      >
+        <div
+          class="mt-5 flex flex-col gap-x-0 gap-y-4 md:mt-0 md:flex-row md:items-center md:justify-center md:gap-x-7 md:gap-y-0"
+        >
+          <div>
+            <a
+              class="relative inline-block text-black before:absolute before:bottom-0.5 before:start-0 before:-z-[1] before:h-1 before:w-full before:bg-lime-400"
+              href="#"
+              aria-current="page"
+              >Work</a
+            >
+          </div>
+          <div>
+            <a class="inline-block text-black hover:text-gray-600" href="#"
+              >Services</a
+            >
+          </div>
+          <div>
+            <a class="inline-block text-black hover:text-gray-600" href="#"
+              >About</a
+            >
+          </div>
+          <div>
+            <a class="inline-block text-black hover:text-gray-600" href="#"
+              >Careers</a
+            >
+          </div>
+          <div>
+            <a class="inline-block text-black hover:text-gray-600" href="#"
+              >Blog</a
+            >
+          </div>
+        </div>
+      </div>
+      <!-- End Collapse -->
+    </nav>
   </header>
+  <!-- ========== END HEADER ========== -->
 </template>
-
-<script setup>
-import { ref, defineProps } from "vue";
-import defaultAvatar from "@/images/airou_small.png";
-
-const props = defineProps({
-  navigationItems: {
-    type: Array,
-    default: () => [
-      { name: "ホーム", href: "/", current: true },
-      { name: "モデルコース", href: "/model-courses", current: false },
-      { name: "スポット", href: "/spots", current: false },
-      { name: "スタンプラリー", href: "/stamp-rallying", current: false },
-    ],
-  },
-  avatarUrl: {
-    type: String,
-    default: defaultAvatar,
-  },
-});
-</script>
