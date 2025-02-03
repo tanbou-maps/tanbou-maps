2a649654af7955260b0788ffbcaf2affcdfae078
Hayate Aoki <aokihayate20030709@gmail.com>
2025-02-03 12:26:41 +0900

    fix: enhance focus styles for links in AppHeaderComponent for improved accessibility

--- app/frontend/components/AppHeaderComponent.vue
+++ app/frontend/components/AppHeaderComponent.vue
@@ -18,7 +18,7 @@
       >
         <a
           href="/sign-out"
-          class="group relative inline-flex items-center justify-center overflow-hidden rounded-xl px-3 py-2 text-sm font-medium"
+          class="group relative inline-flex items-center justify-center overflow-hidden rounded-xl px-3 py-2 text-sm font-medium focus:border-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-100"
         >
           <span
             class="absolute inset-0 h-full w-full bg-gradient-to-br from-gray-300 via-gray-200 to-gray-100"
@@ -30,7 +30,7 @@
         </a>
         <a
           href="#"
-          class="group relative inline-flex items-center justify-center overflow-hidden rounded-xl px-3 py-2 text-sm font-medium"
+          class="group relative inline-flex items-center justify-center overflow-hidden rounded-xl px-3 py-2 text-sm font-medium focus:border-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300"
         >
           <span
             class="absolute inset-0 h-full w-full bg-gradient-to-br from-yellow-500 via-yellow-400 to-yellow-300"
