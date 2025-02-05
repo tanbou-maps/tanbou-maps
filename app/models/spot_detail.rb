8e0558ef2a722459b61990ffd15f3cb334879db8
Xinyi Li <fake@example.com>
2025-02-05 14:21:35 +0900

    perf: 管理者機能のパフォーマンスを向上させるコード変更

--- app/models/spot_detail.rb
+++ app/models/spot_detail.rb
@@ -1,10 +1,10 @@
 class SpotDetail < ApplicationRecord
   belongs_to :spot
 
-  validates :hours_of_operation, presence: true
-  validates :access_info, presence: true
-  validates :contact_info, presence: true
-  validates :website_url, presence: true
-  validates :recommended_season, presence: true
-  validates :entry_fee, presence: true
+  validates :hours_of_operation, presence: true, allow_blank: true
+  validates :access_info, presence: true, allow_blank: true
+  validates :contact_info, presence: true, allow_blank: true
+  validates :website_url, presence: true, allow_blank: true
+  validates :recommended_season, presence: true, allow_blank: true
+  validates :entry_fee, presence: true, allow_blank: true
 end
