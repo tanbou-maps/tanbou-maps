40724a66b0785bc488389094fbd5c46f275a1f63
Hayate Aoki <aokihayate20030709@gmail.com>
2025-02-09 18:44:10 +0900

    feat: remove theme_image_url column from model_courses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_02_09_093451) do
+ActiveRecord::Schema[7.0].define(version: 2025_02_09_094024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -131,7 +131,6 @@ ActiveRecord::Schema[7.0].define(version: 2025_02_09_093451) do
   create_table "model_courses", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
-    t.string "theme_image_url"
     t.boolean "is_public", default: false, null: false
     t.string "public_key", null: false
     t.datetime "created_at", null: false
