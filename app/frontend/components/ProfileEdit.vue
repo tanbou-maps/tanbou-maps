1cea79c70abaff04aa4898fb7f1106251d803dfb
Hayate Aoki <aokihayate20030709@gmail.com>
2025-02-09 23:24:47 +0900

    feat: add ProfileEdit and ProfileShow components for user profile management

---
+++ app/frontend/components/ProfileEdit.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Edit Profile</h1>
+    <form @submit.prevent="updateProfile">
+      <label for="nickname">Nickname:</label>
+      <input type="text" v-model="user.nickname" id="nickname" />
+      <label for="email">Email:</label>
+      <input type="email" v-model="user.email" id="email" />
+      <label for="avatar_image">Avatar Image:</label>
+      <input type="file" @change="onFileChange" id="avatar_image" />
+      <label for="cover_image">Cover Image:</label>
+      <input type="file" @change="onFileChange" id="cover_image" />
+      <button type="submit">Save</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["user"],
+  methods: {
+    updateProfile() {
+      // プロフィール更新ロジック
+    },
+    onFileChange(event) {
+      // ファイル変更ロジック
+    },
+  },
+};
+</script>
---
+++ app/frontend/components/ProfileShow.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ user.nickname }}'s Profile</h1>
+    <p>User ID: {{ user.id }}</p>
+    <p>Account Type: {{ user.account_type }}</p>
+    <img :src="user.avatar_image" alt="Avatar Image" />
+    <img :src="user.cover_image" alt="Cover Image" />
+    <button v-if="isCurrentUser" @click="editProfile">Edit Profile</button>
+    <button v-if="isCurrentUser" @click="deleteAccount">Delete Account</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["user", "isCurrentUser"],
+  methods: {
+    editProfile() {
+      // 編集画面への遷移ロジック
+    },
+    deleteAccount() {
+      // アカウント削除ロジック
+    },
+  },
+};
+</script>
