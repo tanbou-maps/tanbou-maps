524bbd810bcffde5fd756478557d14af4bf10b10
Hayate Aoki <aokihayate20030709@gmail.com>
2025-02-09 18:50:10 +0900

    feat: remove images and related tables from database schema

--- db/migrate/20250209092109_drop_review_images_table.rb
+++
@@ -1,5 +0,0 @@
-class DropReviewImagesTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :review_images
-  end
-end
--- db/migrate/20250209093451_drop_model_course_images_table.rb
+++
@@ -1,5 +0,0 @@
-class DropModelCourseImagesTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :model_course_images
-  end
-end
