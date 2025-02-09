59d6167a74a32d92a7c94006d0b763f679ad8a53
Hayate Aoki <aokihayate20030709@gmail.com>
2025-02-09 23:18:31 +0900

    feat: implement user profile management actions in ProfileController

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,2 +1,38 @@
 class ProfileController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  # GET /profile/:id
+  def show
+    # Logic to display user profile
+  end
+
+  # GET /profile/:id/edit
+  def edit
+    # Logic to display edit form
+  end
+
+  # PATCH/PUT /profile/:id
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Profile was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /profile/:id
+  def destroy
+    @user.destroy
+    redirect_to root_path, notice: 'Account was successfully deleted.'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :avatar_image, :cover_image)
+  end
 end
