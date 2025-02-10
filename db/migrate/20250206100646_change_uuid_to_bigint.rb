d42b25ba8d40cdee91e2628cd9a35669ed0736f6
Hayate Aoki <aokihayate20030709@gmail.com>
2025-02-10 12:18:55 +0900

    refactor: change UUID to bigint for model_courses and update foreign key constraints

--- db/migrate/20250206100646_change_uuid_to_bigint.rb
+++ db/migrate/20250206100646_change_uuid_to_bigint.rb
@@ -2,7 +2,6 @@ class ChangeUuidToBigint < ActiveRecord::Migration[7.0]
   def change
     # 外部キー制約を削除
     remove_foreign_key :course_spots, :model_courses
-    remove_foreign_key :model_course_images, :model_courses
 
     # `model_courses` の `id` を変更
     remove_column :model_courses, :id, :uuid
@@ -12,12 +11,7 @@ class ChangeUuidToBigint < ActiveRecord::Migration[7.0]
     remove_column :course_spots, :model_course_id
     add_column :course_spots, :model_course_id, :bigint, null: false
 
-    # `model_course_images` の `model_course_id` を `bigint` に変更
-    remove_column :model_course_images, :model_course_id
-    add_column :model_course_images, :model_course_id, :bigint, null: false
-
     # 外部キー制約を再追加
-    add_foreign_key :course_spots, :model_courses, column: :model_course_id, on_delete: :cascade
-    add_foreign_key :model_course_images, :model_courses, column: :model_course_id, on_delete: :cascade
+    add_foreign_key :course_spots, :model_courses
   end
 end
